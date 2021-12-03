b3f5a0c8103b12906a664e48d77b2b9008f661f1
yakuza16 <marcin.cincin9010@gmail.com>
2021-12-03 14:43:55 +0100

    addind books and place them in the table works

--- index.js
+++ index.js
@@ -1,32 +1,61 @@
-const books = [];
-const addNewBookBtn = document.getElementById("add");
+const books = []
+const addNewBookBtn = document.getElementById("add")
 
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const prio = document.getElementById("priority");
-const category = document.getElementById("categories");
+const title = document.getElementById("title")
+const table = document.getElementById("table")
+const author = document.getElementById("author")
+const prio = document.getElementById("priority")
+const category = document.getElementById("categories")
 
 const createNewBook = (e) => {
-  e.preventDefault();
+  e.preventDefault()
 
   if (title.value.length < 1 || author.value.length < 3 || prio.value === "") {
-    return;
+    return
   } else {
     const book = {
-      number: books.length === 0 ? books.length + 1 : books.length,
+      number: books.length === 0 ? 1 : books.length + 1,
       title: title.value,
       author: author.value,
       prio: prio.value,
       category: category.value,
-    };
-    books.push(book);
+    }
+    books.push(book)
 
-    title.value = "";
-    prio.value = "";
-    author.value = "";
+    title.value = ""
+    prio.value = ""
+    author.value = ""
 
-    console.log(books);
+    console.log(books)
+
+    newBooksRow(book.number, book.title, book.author, book.prio, book.category)
   }
-};
+}
 
-addNewBookBtn.addEventListener("click", (e) => createNewBook(e));
+const newBooksRow = (num, title, author, priority, category) => {
+  const newBookRow = document.createElement("tr")
+  const numColumn = document.createElement("td")
+  const titleColumn = document.createElement("td")
+  const authorColumn = document.createElement("td")
+  const prioColumn = document.createElement("td")
+  const categoryColumn = document.createElement("td")
+
+  numColumn.textContent = num
+  titleColumn.textContent = title
+  authorColumn.textContent = author
+  prioColumn.textContent = priority
+  categoryColumn.textContent = category
+
+  const bookColumn = [
+    numColumn,
+    titleColumn,
+    authorColumn,
+    prioColumn,
+    categoryColumn,
+  ]
+
+  bookColumn.forEach((item) => newBookRow.appendChild(item))
+  table.appendChild(newBookRow)
+}
+
+addNewBookBtn.addEventListener("click", (e) => createNewBook(e))
