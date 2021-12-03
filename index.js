539a35d37c11b46df417375382974b39c1d3bd76
Marcin Spławiński <64729676+yakuza16@users.noreply.github.com>
2021-12-03 19:31:37 +0100

    working storage but still problem with prority

--- index.js
+++ index.js
@@ -1,49 +1,52 @@
-const books = []
-const addNewBookBtn = document.getElementById("add")
+const table = document.getElementById("table");
+const author = document.getElementById("author");
+const prio = document.getElementById("priority");
+const category = document.getElementById("categories");
+const title = document.getElementById("title");
+const addNewBookBtn = document.getElementById("add");
 
-const title = document.getElementById("title")
-const table = document.getElementById("table")
-const author = document.getElementById("author")
-const prio = document.getElementById("priority")
-const category = document.getElementById("categories")
+const getStoragedBook = () => JSON.parse(localStorage.getItem("books"));
+
+const books = [...getStoragedBook()];
 
 const createNewBook = (e) => {
-  e.preventDefault()
+  e.preventDefault();
 
   if (title.value.length < 1 || author.value.length < 3 || prio.value === "") {
-    return
+    return;
   } else {
     const book = {
-      number: books.length === 0 ? 1 : books.length + 1,
       title: title.value.trim(),
       author: author.value.trim(),
-      prio: prio.value,
+      prio: prio.value > 5 ? 5 : 5,
       category: category.value,
-    }
-    books.push(book)
-    localStorage.setItem(`book${book.number}`, JSON.stringify(book))
-
-    title.value = ""
-    prio.value = ""
-    author.value = ""
-
-    console.log(books)
-
-    newBooksRow(book.number, book.title, book.author, book.prio, book.category)
+    };
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+    newBooksRow(book.title, book.author, book.prio, book.category);
+    title.value = "";
+    prio.value = "";
+    author.value = "";
   }
-}
+};
 
-const newBooksRow = (num, title, author, priority, category) => {
-  const newBookRow = document.createElement("tr")
+const newBooksRow = (title, author, priority, category) => {
+  const newBookRow = document.createElement("tr");
 
   newBookRow.innerHTML = `
-  <td>${num}</td>
   <td>${title}</td>
   <td>${author}</td>
   <td>${priority}</td>
   <td>${category}</td>
-  `
-  table.appendChild(newBookRow)
-}
+  `;
+  table.appendChild(newBookRow);
+};
 
-addNewBookBtn.addEventListener("click", (e) => createNewBook(e))
+addNewBookBtn.addEventListener("click", (e) => createNewBook(e));
+
+window.addEventListener("DOMContentLoaded", () => {
+  books.forEach((book) =>
+    newBooksRow(book.title, book.author, book.prority, book.category)
+  );
+  console.log(books);
+});
